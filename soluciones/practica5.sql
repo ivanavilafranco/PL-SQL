2b2326fa2379eabc7e622051ff8b4594d5a57958
ivanavila <ivanavilafranco@gmail.com>
2022-10-27 11:59:15 +0200

    subida nueva practica 5

--- soluciones/practica5.sql
+++ soluciones/practica5.sql
@@ -1,41 +1,42 @@
---  trigger   tr_departamento
-create or replace noneditionable trigger tr_departamento
-before delete or insert or update of codigo ON departamento
-For each row
-declare
-i number;
-begin
-
--- tabla histo_cambios_departamento
-create table histo_cambios_departamento
-                   ( diayhora date,
-                     texto varchar2(500),
-constraint pk_histo_cambios_departamento primary key(diayhora));
+-- Creación de la tabla histo_cambios_departamento
+CREATE TABLE HISTO_CAMBIOS_DEPARTAMENTO
+(DIAYHORA	DATE,
+TEXTO		VARCHAR2(500),
+CONSTRAINT pk_histo_cambios_departamento PRIMARY KEY(diayhora));
 /
 
--- insertar en tabla departamento
-if inserting then insert into histo_cambios_departamento values(sysdate,'Se ha insertado una fila con datos: '||to_char(:new.codigo)||'-'||:new.nombre);
-elsif deleting then
-                   select count(*) into i
-                   from departamento_empleado
-                   where dept_codigo = :old.codigo; 
+-- Creación del trigger tr_departamento
+CREATE OR REPLACE TRIGGER tr_departamento
+	BEFORE DELETE OR INSERT OR UPDATE OF codigo ON departamento
+	FOR EACH ROW
+DECLARE
+Contador	NUMBER;
+BEGIN
+	IF INSERTING THEN
+		INSERT INTO histo_cambios_departamento
+		VALUES(sysdate,'Se ha insertado una fila con datos: '||TO_CHAR(:new.codigo)||'-'||:new.nombre);
+	ELSIF DELETING THEN
+		SELECT COUNT(*) INTO contador FROM departamento_empleado
+		WHERE dept_codigo = :old.codigo;
 
-if i > 0 then insert into histo_cambios_departamento -- comprobamos el departamento para hacer el borrado
-                   values (sysdate,'Se ha intentado borrar el departamento: '||to_char(:old.codigo));
-
-else insert into histo_cambios_departamento values (sysdate,'Se ha borrado el departamento: '||to_char(:old.codigo));
-end if;
-else
-                   select count(*) into i
-                   from departamento_empleado
-                   where dept_codigo = :old.codigo; 
-
-if i > 0 then --la tabla departamento debe actualizarse
-                   update departamento_empleado
-                   set dept_codigo = :new.codigo
-                   where dept_codigo = :old.codigo;
-end if;
---ultimo caso, si existen datos o no, se introducirÃ¡ registro en la tabla HISTO_CAMBIOS_DEPARTAMENTO
-       insert into histo_cambios_departamento values (sysdate,'Se ha actualizado el departamento '||to_char(:old.codigo)||' al valor '||to_char(:new.codigo));
-end if;
-end;/
+		IF contador > 0 THEN
+			INSERT INTO histo_cambios_departamento
+			VALUES (sysdate,'Se ha intentado borrar el departamento: '||TO_CHAR(:old.codigo));
+		ELSE
+			INSERT INTO histo_cambios_departamento
+			VALUES (sysdate,'Se ha borrado el departamento: '||TO_CHAR(:old.codigo));
+		END IF;
+	ELSE
+		SELECT COUNT(*) INTO contador FROM departamento_empleado
+		WHERE dept_codigo = :old.codigo;
+		
+		IF contador > 0 THEN
+			UPDATE departamento_empleado
+			SET dept_codigo = :new.codigo
+			WHERE dept_codigo = :old.codigo;
+		END IF;
+		INSERT INTO histo_cambios_departamento
+		VALUES (sysdate,'Se ha actualizado el departamento '||TO_CHAR(:old.codigo)||' al valor '||TO_CHAR(:new.codigo));
+	END IF;
+END;
+/
